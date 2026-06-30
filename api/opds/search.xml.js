5f0c8ccd68ed6e656e136d8d534faff993ab5c02
Mahim365 <amahim365@gmail.com>
2026-07-01 02:50:07 +0600

    Refactor handler to improve readability and update XML

--- api/opds/search.xml.js
+++ api/opds/search.xml.js
@@ -1,14 +1,16 @@
 export default function handler(req, res) {
-  const proto = req.headers['x-forwarded-proto'] || 'https';
-  const host = req.headers['x-forwarded-host'] || req.headers.host || 'localhost';
-  const baseUrl = `${proto}://${host}`;
-  
+  const protocol = req.headers['x-forwarded-proto'] || 'https';
+  const host = req.headers['x-forwarded-host'] || req.headers.host;
+  const baseUrl = `${protocol}://${host}`;
+
   res.setHeader('Content-Type', 'application/opensearchdescription+xml; charset=utf-8');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
   res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
 <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
-  <ShortName>Free Books</ShortName>
+  <ShortName>Book Search</ShortName>
   <Description>Search Anna's Archive and Z-Library</Description>
-  <Url type="application/atom+xml;profile=opds-catalog;kind=acquisition"
-       template="${baseUrl}/api/opds?q={searchTerms}&amp;source=all"/>
+  <InputEncoding>UTF-8</InputEncoding>
+  <Url type="application/atom+xml;profile=opds-catalog;kind=acquisition" template="${baseUrl}/opds?q={searchTerms}&amp;source=all"/>
 </OpenSearchDescription>`);
 }
